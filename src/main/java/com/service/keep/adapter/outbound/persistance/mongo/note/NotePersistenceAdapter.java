4fe0c91b8c42d651fa1936d2efec8b985b3da3b9
bhupendrasambare <bhupendrasam1404@gmail.com>
2025-12-12 22:55:22 +0530

    file added

--- src/main/java/com/service/keep/adapter/outbound/persistance/mongo/note/NotePersistenceAdapter.java
+++ src/main/java/com/service/keep/adapter/outbound/persistance/mongo/note/NotePersistenceAdapter.java
@@ -6,5 +6,18 @@
  **/
 package com.service.keep.adapter.outbound.persistance.mongo.note;
 
-public class NotePersistenceAdapter {
+import com.service.keep.domain.model.Note;
+import com.service.keep.domain.port.outbound.NoteRepositoryPort;
+import com.service.keep.domain.valueobject.NoteId;
+import com.service.keep.domain.valueobject.UserId;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Repository
+@RequiredArgsConstructor
+public class NotePersistenceAdapter implements NoteRepositoryPort {
 }
