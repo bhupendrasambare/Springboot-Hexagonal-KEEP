a6c0b2e73b610eae8df6593036bd3a19f0110dc2
bhupendrasambare <bhupendrasam1404@gmail.com>
2025-12-16 22:55:44 +0530

    entity added

--- src/main/java/com/service/keep/adapter/outbound/persistance/mongo/tags/TagDocument.java
+++ src/main/java/com/service/keep/adapter/outbound/persistance/mongo/tags/TagDocument.java
@@ -6,5 +6,29 @@
  **/
 package com.service.keep.adapter.outbound.persistance.mongo.tags;
 
+import lombok.Getter;
+import lombok.Setter;
+import nonapi.io.github.classgraph.json.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@Document(collation = "tags")
 public class TagDocument {
+
+
+    @Id
+    private String id;
+
+    private String userId;
+
+    private String color;
+    private String imageUri;
+    private String userName;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
 }
