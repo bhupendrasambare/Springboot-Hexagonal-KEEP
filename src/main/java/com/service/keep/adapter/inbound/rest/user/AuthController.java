edc29222ba5c7e896d1cc83a603282860fddd4a8
bhupendrasambare <bhupendrasam1404@gmail.com>
2025-12-28 22:51:11 +0530

    auth signup

--- src/main/java/com/service/keep/adapter/inbound/rest/user/AuthController.java
+++ src/main/java/com/service/keep/adapter/inbound/rest/user/AuthController.java
@@ -6,5 +6,26 @@
  **/
 package com.service.keep.adapter.inbound.rest.user;
 
+import com.service.keep.application.dto.request.SignUpRequest;
+import com.service.keep.application.dto.response.AuthResponse;
+import com.service.keep.application.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public AuthResponse signUp(@Valid @RequestBody SignUpRequest request){
+        return this.authService.signUp(request);
+    }
+
 }
