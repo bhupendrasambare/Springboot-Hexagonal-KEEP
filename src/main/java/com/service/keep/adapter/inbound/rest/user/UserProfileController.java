2d7aa8ca73101be3e2b793975d9cff10db36898a
bhupendrasambare <bhupendrasam1404@gmail.com>
2025-12-30 23:12:45 +0530

    apis added

--- src/main/java/com/service/keep/adapter/inbound/rest/user/UserProfileController.java
+++ src/main/java/com/service/keep/adapter/inbound/rest/user/UserProfileController.java
@@ -6,5 +6,33 @@
  **/
 package com.service.keep.adapter.inbound.rest.user;
 
+import com.service.keep.application.dto.request.ChangePasswordRequest;
+import com.service.keep.application.dto.request.UpdateProfileRequest;
+import com.service.keep.application.dto.response.UserResponse;
+import com.service.keep.application.service.UserProfileService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
 public class UserProfileController {
+
+    private final UserProfileService userProfileService;
+
+    @GetMapping("/me")
+    public UserResponse profile(){
+        return this.userProfileService.getProfile();
+    }
+
+    @PutMapping("/update")
+    public UserResponse update(@RequestBody UpdateProfileRequest request) {
+        return userProfileService.updateProfile(request);
+    }
+
+    @PutMapping("/change-password")
+    public void changePassword(@RequestBody ChangePasswordRequest request) {
+        userProfileService.updatePassword(request);
+    }
+
 }
