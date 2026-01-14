9b571ccfbe835393a1be3f9523915cc35560b1c7
bhupendrasambare <bhupendrasam1404@gmail.com>
2026-01-14 22:37:08 +0530

    tags controller

--- src/main/java/com/service/keep/adapter/inbound/rest/tags/TagsController.java
+++ src/main/java/com/service/keep/adapter/inbound/rest/tags/TagsController.java
@@ -8,8 +8,12 @@ package com.service.keep.adapter.inbound.rest.tags;
 
 import com.service.keep.application.dto.request.TagCreateRequest;
 import com.service.keep.application.dto.response.TagResponse;
+import com.service.keep.application.mapper.TagMapper;
 import com.service.keep.application.service.TagsService;
+import com.service.keep.domain.port.inbound.TagsUseCase;
+import com.service.keep.domain.port.outbound.AuthenticatedUserPort;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,15 +21,31 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class TagsController {
 
-    private final TagsService tagsService;
+    private final TagsUseCase tagsService;
+
+    private final AuthenticatedUserPort authenticatedUserPort;
+
+    // TEMP – replace with SecurityContext later
+    private String getUserId() {
+        return authenticatedUserPort.getCurrentUserId().getValue();
+    }
 
     @PostMapping
-    public TagResponse create(@RequestBody TagCreateRequest request) {
-        return tagsService.create(request);
+    public ResponseEntity<TagResponse> create(@RequestBody TagCreateRequest request) {
+        return ResponseEntity.ok(
+                TagMapper.toResponse(
+                        tagsService.create(
+                                this.getUserId(),
+                                request.getName(),
+                                request.getName(),
+                                request.getImageUri())
+                )
+        );
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable String id) {
-        tagsService.delete(id);
+    public ResponseEntity<?> delete(@PathVariable String id) {
+        tagsService.delete(getUserId(),id);
+        return ResponseEntity.ok(null);
     }
 }
