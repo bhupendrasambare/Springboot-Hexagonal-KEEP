5153b7088bea0140ab0e20060decfe56d3d7f170
bhupendrasambare <bhupendrasam1404@gmail.com>
2026-01-31 22:51:33 +0530

    ui changes in nav

--- frontent/src/components/LocalNav.jsx
+++ frontent/src/components/LocalNav.jsx
@@ -1,16 +1,27 @@
+import { useEffect, useState } from 'react';
+import { Fade } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
-import { BsGrid3X3Gap } from 'react-icons/bs';
-import { CiGrid2H } from 'react-icons/ci';
+import { BsGrid3X3Gap, BsViewStacked, BsGrid } from "react-icons/bs";
 import { GiHamburgerMenu } from 'react-icons/gi';
+import { IoMdCloudOutline } from 'react-icons/io';
 import { MdOutlineRefresh, MdOutlineSettings, MdOutlineSupervisedUserCircle } from 'react-icons/md';
 
-function LocalNav() {
+function LocalNav({sidebarActive, setSidebarActive,showRow, setShowRow}) {
+
+  const [refresh, setRefresh] = useState(false);
+
+  useEffect(()=>{
+    const timerId = setTimeout(() => {
+      setRefresh(false)
+    }, 1000)
+  }, [refresh])
+
   return (
     <Navbar className="bg-body-tertiary">
       <div className='mx-1p d-flex w-100'>
         <Navbar.Brand href="#home">
-          <GiHamburgerMenu size={25} className='me-4'/>
+          <GiHamburgerMenu onClick={() =>setSidebarActive(!sidebarActive)} size={25} className='me-4'/>
           Keep
         </Navbar.Brand>
         <form action="" className="form-control w-50 p-0">
@@ -18,8 +29,18 @@ function LocalNav() {
         </form>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end gap-4">
-          <MdOutlineRefresh size={25} />
-          <CiGrid2H size={25} />
+          {
+            (refresh != true)? 
+            <MdOutlineRefresh onClick={()=> setRefresh(true)} size={25} />:
+            <IoMdCloudOutline size={25} />
+          }
+          
+          
+          {
+            (showRow != true)? 
+            <BsViewStacked onClick={()=> setShowRow(true)} size={25} />:
+            <BsGrid onClick={()=> setShowRow(false)} size={25} />
+          }
           <MdOutlineSettings size={25} />
           <BsGrid3X3Gap size={25} />
           <MdOutlineSupervisedUserCircle size={25} />
--- frontent/src/homepage/HomePage.jsx
+++ frontent/src/homepage/HomePage.jsx
@@ -3,11 +3,16 @@ import LocalNav from '../components/LocalNav'
 
 function HomePage() {
     const[sidebarActive, setSidebarActive] = useState(false)
+    const[showRow, setShowRow] = useState(false)
 
   return (
     <>
-    <LocalNav sidebarActive={sidebarActive} setSidebarActive={setSidebarActive} />
-    
+    <LocalNav
+     sidebarActive={sidebarActive}
+     setSidebarActive={setSidebarActive}
+     showRow={showRow}
+     setShowRow={setShowRow} />
+    {sidebarActive? "Active":"in_actiove"}
     </>
   )
 }
