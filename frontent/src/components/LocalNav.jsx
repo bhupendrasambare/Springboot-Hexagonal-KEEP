4141dc7fe4dd1f931348930c7856bc2b18a0d8de
bhupendrasambare <bhupendrasam1404@gmail.com>
2026-02-21 23:13:29 +0530

    login details

--- frontent/src/components/LocalNav.jsx
+++ frontent/src/components/LocalNav.jsx
@@ -1,8 +1,10 @@
+import Modal from "react-bootstrap/Modal";
 import { useEffect, useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import { BsGrid3X3Gap, BsViewStacked, BsGrid } from "react-icons/bs";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IoMdCloudOutline } from "react-icons/io";
+import {useAuth} from "../store/AuthContext"
 import {
   MdOutlineRefresh,
   MdOutlineSupervisedUserCircle,
@@ -10,9 +12,15 @@ import {
   MdOutlineLightMode,
   MdOutlineSettings,
 } from "react-icons/md";
+import { useNavigate } from "react-router-dom";
+import { Button } from "react-bootstrap";
 
 function LocalNav({ sidebarActive, setSidebarActive, showRow, setShowRow }) {
   const [refresh, setRefresh] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+
+  const { user, logout } = useAuth();
+  const navigate = useNavigate();
 
   useEffect(() => {
 
@@ -21,7 +29,15 @@ function LocalNav({ sidebarActive, setSidebarActive, showRow, setShowRow }) {
       }, 1000)
   }, [refresh])
 
+
+  const handleLogout = () => {
+    logout();
+    setShowModal(false);
+    navigate("/");
+  };
+
   return (
+    <>
     <Navbar className="bg-body-tertiary bg-black" data-bs-theme="dark">
       <div className='mx-1p d-flex w-100'>
         <Navbar.Brand href="#home">
@@ -49,11 +65,41 @@ function LocalNav({ sidebarActive, setSidebarActive, showRow, setShowRow }) {
           }
           <MdOutlineSettings size={25} />
           <BsGrid3X3Gap size={25} />
-          <MdOutlineSupervisedUserCircle size={25} />
+          <MdOutlineSupervisedUserCircle size={25} onClick={()=> setShowModal(true)} />
 
         </Navbar.Collapse>
       </div>
     </Navbar>
+
+    {/* USER MODAL */}
+      <Modal show={showModal} onHide={() => setShowModal(false)} centered >
+        <Modal.Header closeButton className="bg-dark text-light" >
+          <Modal.Title>User Profile</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body className="bg-dark text-light">
+          {user ? (
+            <>
+              <p><strong>Username:</strong> {user.userName}</p>
+              <p><strong>First Name:</strong> {user.firstName}</p>
+              <p><strong>Last Name:</strong> {user.lastName}</p>
+              <p><strong>Email:</strong> {user.email}</p>
+            </>
+          ) : (
+            <p>No user data found.</p>
+          )}
+        </Modal.Body>
+
+        <Modal.Footer className="bg-dark text-light">
+          <Button variant="secondary" onClick={() => setShowModal(false)}>
+            Close
+          </Button>
+          <Button variant="danger" onClick={handleLogout}>
+            Logout
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 }
 
