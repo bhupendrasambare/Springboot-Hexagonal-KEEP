49a1d693b8c888648e8698319c7ea12a988f7147
bhupendrasambare <bhupendrasam1404@gmail.com>
2026-03-02 20:12:14 +0530

    useEffect added

--- frontent/src/sections/Notes.jsx
+++ frontent/src/sections/Notes.jsx
@@ -1,15 +1,25 @@
 import axios from 'axios';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export const Notes = () => {
 
   const [notesList, setNotesList] = useState([]);
 
+  useEffect({
+
+  },[])
+
+  const getNotes = () =>{
+    
+  }
+
   return (
     <div>
       <h1>Notes</h1>
       <p>Recent notes</p>
-      <div className="container"></div>
+      <div className="container">
+
+      </div>
     </div>
   )
 }
