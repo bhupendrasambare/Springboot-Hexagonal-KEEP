91b4ff4c6f9ad65e451e975b89573be5b46611e7
bhupendrasambare <bhupendrasam1404@gmail.com>
2026-03-12 22:53:22 +0530

    archive page added

--- frontent/src/sections/Archive.jsx
+++ frontent/src/sections/Archive.jsx
@@ -1,11 +1,39 @@
-import { Component } from 'react'
+import { useEffect, useState } from "react";
+import { getArchiveNotesApi } from "../api/notesService";
 
-export class Archive extends Component {
-  render() {
-    return (
-      <div>Archive</div>
-    )
-  }
-}
+export const Notes = () => {
+  const [archiveNotesList, setArchiveNotes] = useState([]);
 
-export default Archive
+  useEffect(() => {
+    loadArchiveNotes();
+  }, []);
+
+  const loadArchiveNotes = async () => {
+    try {
+      const data = await getArchiveNotesApi();
+      setArchiveNotes(data);
+    } catch (error) {
+      console.error("Error fetching notes:", error);
+    }
+  };
+
+  return (
+    <div className="w-100">
+
+      <h1 className="text-secondary">Archive Notes</h1>
+
+      {/* Notes List */}
+      <div className="container">
+        {archiveNotesList.map((note) => (
+          <div key={note.id} className="card p-3 mb-2">
+            <h5>{note.title}</h5>
+            <p>{note.description}</p>
+          </div>
+        ))}
+      </div>
+
+    </div>
+  );
+};
+
+export default Notes;
