e57970b838061406756ea199125fbda8ea92f9f3
bhupendrasambare <bhupendrasam1404@gmail.com>
2026-03-16 22:50:17 +0530

    deleted entried added

--- frontent/src/sections/Bin.jsx
+++ frontent/src/sections/Bin.jsx
@@ -1,11 +1,40 @@
-import { Component } from 'react'
+import { useEffect, useState } from "react";
+import { getArchiveNotesApi, getTrashNotesApi } from "../api/notesService";
 
-export class Bin extends Component {
-  render() {
-    return (
-      <div>Bin</div>
-    )
-  }
-}
+export const Bin = () => {
+  const [archiveNotesList, setArchiveNotes] = useState([]);
 
-export default Bin
+  useEffect(() => {
+    loadArchiveNotes();
+  }, []);
+
+  const loadArchiveNotes = async () => {
+    try {
+      const data = await getTrashNotesApi();
+      setArchiveNotes(data);
+    } catch (error) {
+      console.error("Error fetching notes:", error);
+    }
+  };
+
+
+  return (
+    <div className="w-100">
+
+      <h1 className="text-secondary">Archive Notes</h1>
+
+      {/* Notes List */}
+      <div className="container card">
+        {archiveNotesList.map((note) => (
+          <div key={note.id} className="card p-3 mb-2">
+            <h5>{note.title}</h5>
+            <p>{note.description}</p>
+          </div>
+        ))}
+      </div>
+
+    </div>
+  );
+};
+
+export default Bin;
