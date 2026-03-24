4172f40382dc8015c2e717582c8f943fc332667e
bhupendrasambare <bhupendrasam1404@gmail.com>
2026-03-24 23:07:30 +0530

    ui decission on cards

--- frontent/src/components/NotesCard.jsx
+++ frontent/src/components/NotesCard.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { IoTrashBinOutline } from 'react-icons/io5'
+import { MdOutlineArchive, MdOutlinePushPin } from 'react-icons/md'
 
 function NotesCard({noteData}) {
   return (
     <div className='card note-cards p-2 m-3'>
         <h1 className='fw-bold fs-4'>{noteData.title}</h1>
         <p className='fs-6'>{noteData.description}</p>
+
+        <div className="footer">
+          <MdOutlineArchive className='card-circle-icon' />
+          <IoTrashBinOutline className='card-circle-icon' />
+          <MdOutlinePushPin className='card-circle-icon' />
+        </div>
     </div>
   )
 }
