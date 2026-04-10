0a647702a165c80fcb1c9803536f612442914bee
bhupendrasambare <bhupendrasam1404@gmail.com>
2026-04-10 23:00:26 +0530

    scheduler added

---
+++ src/main/java/com/service/keep/infrastructure/scheduler/NoteMetaDataScheduler.java
@@ -0,0 +1,18 @@
+/**
+ * author @bhupendrasambare
+ * Date   :10/04/26
+ * Time   :10:59 pm
+ * Project:Keep
+ **/
+package com.service.keep.infrastructure.scheduler;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NoteMetaDataScheduler {
+
+    @Scheduled(fixedDelay = 10000)
+    public void updateMetaData(){}
+
+}
