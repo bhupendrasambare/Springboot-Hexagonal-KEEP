eb06d0277ef5f17fba2f517ded51a961dc37d823
bhupendrasambare <bhupendrasam1404@gmail.com>
2026-04-12 23:29:49 +0530

    async todo added

--- src/main/java/com/service/keep/infrastructure/scheduler/NoteMetaDataScheduler.java
+++ src/main/java/com/service/keep/infrastructure/scheduler/NoteMetaDataScheduler.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class NoteMetaDataScheduler {
 
+    // TODO: change generateMetadata function to async and call it from scheduler
     @Scheduled(fixedDelay = 1000)
     public void updateMetaData(){}
 
