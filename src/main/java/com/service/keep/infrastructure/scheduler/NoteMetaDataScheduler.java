16034fe0d1c8e1bd5c4807639fbb06e1eac02abc
bhupendrasambare <bhupendrasam1404@gmail.com>
2026-04-14 20:36:57 +0530

    async method added

--- src/main/java/com/service/keep/infrastructure/scheduler/NoteMetaDataScheduler.java
+++ src/main/java/com/service/keep/infrastructure/scheduler/NoteMetaDataScheduler.java
@@ -6,13 +6,24 @@
  **/
 package com.service.keep.infrastructure.scheduler;
 
+import com.service.keep.domain.model.Note;
+import com.service.keep.domain.port.outbound.AiSearchPort;
+import lombok.AllArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
+@AllArgsConstructor
 public class NoteMetaDataScheduler {
 
+    private final AiSearchPort aiSearchPort;
+
     @Scheduled(fixedDelay = 1000)
-    public void updateMetaData(){}
+    public void updateMetaData(){
+
+
+    }
 
 }
