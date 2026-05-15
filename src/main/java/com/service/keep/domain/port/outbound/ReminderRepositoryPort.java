c0f99519d05cbbc66634ea5c4b16cb498a8338f4
bhupendrasambare <bhupendrasam1404@gmail.com>
2026-05-15 21:07:29 +0530

    defination added

--- src/main/java/com/service/keep/domain/port/outbound/ReminderRepositoryPort.java
+++ src/main/java/com/service/keep/domain/port/outbound/ReminderRepositoryPort.java
@@ -6,5 +6,35 @@
  **/
 package com.service.keep.domain.port.outbound;
 
+import com.service.keep.domain.model.Reminder;
+import com.service.keep.domain.valueobject.ReminderId;
+import com.service.keep.domain.valueobject.UserId;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface ReminderRepositoryPort {
+
+    Reminder save(Reminder reminder);
+
+    Page<Reminder> findByUserId(
+            UserId userId,
+            boolean pinned,
+            boolean archived,
+            boolean trashed,
+            String keyword,
+            int page,
+            int pageSize
+    );
+
+
+    Optional<Reminder> findById(ReminderId id);
+
+    List<Reminder> findAllByUserId(UserId userId);
+
+    Page<Reminder> findAllByMetaDataFlag(Boolean metaDataFlag, Pageable pageable);
+
+    List<Reminder> searchByAi(UserId userId, List<String> keywords, List<String> tags, String title);
 }
