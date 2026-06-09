fee5366f54a2e0e5b107813e4df8636778ffed4d
bhupendrasambare <bhupendrasam1404@gmail.com>
2026-06-09 21:55:01 +0530

    remove unwanted import

--- src/main/java/com/service/keep/KeepApplication.java
+++ src/main/java/com/service/keep/KeepApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
